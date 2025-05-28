e9e5275a398362323fc195ea59517563d9dca986
RoaringRoyel <minhazulroyel001@gmail.com>
2025-05-28 14:01:48 +0600

    Evaluation Matrics added [ Weighted Avg ]

--- royel_flwr/server_app.py
+++ royel_flwr/server_app.py
@@ -1,9 +1,17 @@
 """royel-flwr: A Flower / PyTorch app."""
 
-from flwr.common import Context, ndarrays_to_parameters
+from typing import List, Tuple #for weghted_average callback function
+from flwr.common import Context, ndarrays_to_parameters, Metrics
 from flwr.server import ServerApp, ServerAppComponents, ServerConfig
 from flwr.server.strategy import FedAvg
 from royel_flwr.task import Net, get_weights
+
+def weighted_average(metrics: List[tuple[int, Metrics]]) -> Metrics:
+    """ A function that aggregates metrics from clients. """
+    accuracies = [num_examples * m["accuracy"] for num_examples,m in metrics]
+    total_examples = sum(num_examples for num_examples, _ in metrics)
+    return {"accuracy": sum(accuracies) / total_examples}
+
 
 
 def server_fn(context: Context):
@@ -21,6 +29,7 @@ def server_fn(context: Context):
         fraction_evaluate=1.0,
         min_available_clients=8,
         initial_parameters=parameters,
+        evaluate_metrics_aggregation_fn=weighted_average,  # Custom aggregation function
     )
     config = ServerConfig(num_rounds=num_rounds)
 
